c26bc950020e342c69cefaf56e92c215200dc81a
Carissa Niederer <cniederer@gmail.com>
2021-02-28 18:40:46 -0600

    Turn App into a class function to prepare for toggling the details

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 const openDetails = (id) => {
@@ -14,37 +15,39 @@ function ClickablePokemon(props) {
   )
 };
 
-function App() {
-  const POKEMON = [
-    { id: 58, name: 'Growlithe', spriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/58.png' },
-    { id: 59, name: 'Arcanine', spriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png' }
-  ];
-  
-  return (
-    <div className="App">
-      <header>
-        <h1>Pokedex - Kanto region</h1>
-      </header>
-      <div className="pokedex-layout">
-        <section className="section-scrollable">
-          <ul className="reset horizontal pokemon-list">
-            {POKEMON.map(poke => (
-              <ClickablePokemon key={poke.id} {...poke} />
-            ))}
-          </ul>
-        </section>
-        <aside className="aside-fixed pokemon-details">
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/58.png" alt="Growlithe" />
-          <h2>#058 Growlithe</h2>
-          <dl>
-            <dt>Type</dt>
-            <dd>Fire</dd>
-          </dl>
-          <p>Very protective of its territory. It will bark andbite to repel intruders from its space.</p>
-        </aside>
+class App extends React.Component {
+  render() {
+    const POKEMON = [
+      { id: 58, name: 'Growlithe', spriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/58.png' },
+      { id: 59, name: 'Arcanine', spriteUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png' }
+    ];
+
+    return (
+      <div className="App">
+        <header>
+          <h1>Pokedex - Kanto region</h1>
+        </header>
+        <div className="pokedex-layout">
+          <section className="section-scrollable">
+            <ul className="reset horizontal pokemon-list">
+              {POKEMON.map(poke => (
+                <ClickablePokemon key={poke.id} {...poke} />
+              ))}
+            </ul>
+          </section>
+          <aside className="aside-fixed pokemon-details">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/58.png" alt="Growlithe" />
+            <h2>#058 Growlithe</h2>
+            <dl>
+              <dt>Type</dt>
+              <dd>Fire</dd>
+            </dl>
+            <p>Very protective of its territory. It will bark andbite to repel intruders from its space.</p>
+          </aside>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
